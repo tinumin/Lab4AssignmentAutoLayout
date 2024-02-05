f7edd89a10b78e3aa40b9e2900eca573e4023836
Tinu <tinukudassanadu@gmail.com>
2024-02-05 00:25:01 -0500

    Initial Commit

---
+++ Lab4Assignment/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Lab4Assignment
+//
+//  Created by user237087 on 1/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Assignment 3 Auto Layout
+    }
+}
